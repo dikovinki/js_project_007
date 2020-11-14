152d41cd0cb2112330e2e77864883ba2af428195
Andrey <andrey.zhelkovsky@gmail.com>
2020-11-14 18:31:08 -0500

    updating index & js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ for (let i = 0; i < arr.length; i++) {
 for (let value of arr) {
     console.log(value);
 }
-// Самый используемый в метод FOR EACH работает с call-back functions
+// Самый используемый в JS метод FOR EACH работает с call-back functions
 // т.к. ее можно настроить 3 аргумента
 // item - элемент котор мы будем перебирать (можно назвать как угодно)
 // i - порядковый номер
@@ -35,3 +35,33 @@ arra.forEach(function(item, i, arra) {
     console.log(`${i}: ${item} внутри массива ${arra}`);
 });
 
+const description = {
+    name: 'WTF?',
+    height: 2,
+    structure: 'cube',
+    values: {
+        background: 'multicolor',
+        borderWidth: 5,
+        numberOf: 42
+
+    },
+    makeTest: function() {
+        console.log("TEST TEST TEST");
+    }
+};
+
+// создаем свой сбственный метод
+description.makeTest();
+// встроенный метод в языке
+console.log(Object.keys(description).length)
+
+var u=0, age = 1;
+
+while (1) {
+  if (u==true) {
+    console.log('Happy Birthday');
+    break;
+  }
+  else
+    u++;
+}
